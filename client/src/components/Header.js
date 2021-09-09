b79c5bbe905487ad772d47790cb87a11ca0e66ef
Nimish Shah <nimishshah2000@gmail.com>
2021-09-09 11:47:29 +0530

    add redeem facility

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,10 +16,65 @@ import {
   Stack,
   useColorMode,
   Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  FormControl,
+  Input,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon, SunIcon, MoonIcon } from '@chakra-ui/icons';
 import { useWallet } from '../helper/WalletContext';
 import { CONTRACT_ADDRESS, wallet } from '../helper/tezos';
+
+const Redeem = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const { tokenId, amount } = e.target.elements;
+    console.log(tokenId.value, amount.value);
+    const contract = await wallet.at(CONTRACT_ADDRESS);
+
+    await contract.methods
+      .redeemTokens(parseInt(amount.value), parseInt(tokenId.value))
+      .send();
+  };
+
+  return (
+    <>
+      <MenuItem onClick={onOpen}>Redeem</MenuItem>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Redeem Token</ModalHeader>
+          <ModalBody>
+            <form onSubmit={submit}>
+              <FormControl>
+                <Input
+                  required
+                  type="number"
+                  name="tokenId"
+                  placeholder="Token ID"
+                ></Input>
+              </FormControl>
+              <FormControl>
+                <Input
+                  required
+                  type="number"
+                  name="amount"
+                  placeholder="Amount"
+                />
+              </FormControl>
+              <Button type="submit">Redeem</Button>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 export default function Header({ links = [] }) {
   const history = useHistory();
@@ -46,12 +101,14 @@ export default function Header({ links = [] }) {
           display={{ md: 'none' }}
           onClick={isOpen ? onClose : onOpen}
         />
-		
+
         <HStack spacing={8} alignItems={'center'}>
           <Box></Box>
         </HStack>
-		 <Text fontSize="3xl" colorScheme = "blue" fontWeight="bold">Predictor</Text>
-        
+        <Text fontSize="3xl" colorScheme="blue" fontWeight="bold">
+          Predictor
+        </Text>
+
         <Flex alignItems={'center'}>
           <IconButton
             marginRight="10px"
@@ -79,6 +136,7 @@ export default function Header({ links = [] }) {
                   </MenuItem>
                   <MenuItem onClick={whiteListProposer}>Whitelist Me</MenuItem>
                   <MenuItem onClick={disconnect}>Disconnect</MenuItem>
+                  <Redeem />
                 </MenuList>
               </Menu>
             )}
