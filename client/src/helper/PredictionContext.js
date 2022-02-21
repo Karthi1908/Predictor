e3b06b1988ad337682644cba956b4debd3b7a008
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-02-21 20:29:57 +0800

    Delete client/src directory

--- client/src/helper/PredictionContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PredictionContext = React.createContext({
-  predictions: {},
-  predictionsArray: [],
-  updatePredictions: () => {},
-});
-
-export default PredictionContext;
