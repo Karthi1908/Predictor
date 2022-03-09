66f55b2c2c259a82c95f89b5dca4535d942df340
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-03-10 01:05:15 +0800

    Update tezos.js
    Changes to Contract

--- client/src/helper/tezos.js
+++ client/src/helper/tezos.js
@@ -1,7 +1,7 @@
 import { NetworkType } from '@airgap/beacon-sdk';
 import { BeaconWallet } from '@taquito/beacon-wallet';
 import { TezosToolkit } from '@taquito/taquito';
-const CONTRACT_ADDRESS = 'KT1BBsZasAeivA6gh7VHDC6WtiyFScv3sSpz';
+const CONTRACT_ADDRESS = 'KT1NGjzawHFbwFGBdYJWvPbQ7QHLnthvLkqV';
 
 const Tezos = new TezosToolkit('https://hangzhounet.smartpy.io');
 
