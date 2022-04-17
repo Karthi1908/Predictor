b8e583784dca48016c8369501d230cfb6b7f5bb6
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-04-17 20:05:15 +0800

    Add files via upload

--- client/src/helper/tezos.js
+++ client/src/helper/tezos.js
@@ -1,14 +1,16 @@
 import { NetworkType } from '@airgap/beacon-sdk';
 import { BeaconWallet } from '@taquito/beacon-wallet';
 import { TezosToolkit, MichelCodecPacker, compose } from '@taquito/taquito';
-import { Tzip12Module, tzip12 } from "@taquito/tzip12";
+//import { Tzip12Module, tzip12 } from "@taquito/tzip12";
 //import { Tzip16Module, tzip16, bytes2Char } from "@taquito/tzip16";
 
-//const CONTRACT_ADDRESS = 'KT1BBsZasAeivA6gh7VHDC6WtiyFScv3sSpz';
-const CONTRACT_ADDRESS = 'KT1NgQTehmVZEqUV6j7Qjzmnq3VKhd6e6o3B';
-const TOKEN_ADDRESS = 'KT1JxbHQNs8gVVQfHV9LyMNhQJCbNXfr7moy';
+//const CONTRACT_ADDRESS = 'KT1NgQTehmVZEqUV6j7Qjzmnq3VKhd6e6o3B';
+//const TOKEN_ADDRESS = 'KT1JxbHQNs8gVVQfHV9LyMNhQJCbNXfr7moy';
+const CONTRACT_ADDRESS = 'KT1DwGe6b1x3iqCGMgrFykasxEErt986Rgox';
+const TOKEN_ADDRESS = 'KT1BvYkPGPau7xDSRxLVz7DQpuWZfrXBLL27';
 
-const Tezos = new TezosToolkit('https://hangzhounet.smartpy.io');
+//const Tezos = new TezosToolkit('https://hangzhounet.smartpy.io');
+const Tezos = new TezosToolkit('https://ithacanet.smartpy.io');
 Tezos.setPackerProvider(new MichelCodecPacker());
 
 const ContractProvider = Tezos.contract;
