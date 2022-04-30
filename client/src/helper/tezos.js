f750422a9247e744b2ce75259507118fe42ba727
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-04-30 15:30:10 +0800

    Update tezos.js

--- client/src/helper/tezos.js
+++ client/src/helper/tezos.js
@@ -4,10 +4,13 @@ import { TezosToolkit, MichelCodecPacker, compose } from '@taquito/taquito';
 //import { Tzip12Module, tzip12 } from "@taquito/tzip12";
 //import { Tzip16Module, tzip16, bytes2Char } from "@taquito/tzip16";
 
-const CONTRACT_ADDRESS = 'KT1BavDyS4Qkv2xeRTzy5mf37oJcmHTSgtHR';
-const TOKEN_ADDRESS = 'KT19Spcv2j1ktJd5EBTkg4XbnaEeRD1tWmhW';
-//const CONTRACT_ADDRESS = 'KT1DwGe6b1x3iqCGMgrFykasxEErt986Rgox';
-//const TOKEN_ADDRESS = 'KT1BvYkPGPau7xDSRxLVz7DQpuWZfrXBLL27';
+const CONTRACT_ADDRESS = 'KT1XnuvJeej9FgPjen3eQeKHUtP97o8rmzkD';
+const TOKEN_ADDRESS = 'KT1GpoecQWgr7gBDuNwnTPGzDeCTK7WJM28L';
+
+//const CONTRACT_ADDRESS = 'KT1BavDyS4Qkv2xeRTzy5mf37oJcmHTSgtHR'; -- v2
+//const TOKEN_ADDRESS = 'KT19Spcv2j1ktJd5EBTkg4XbnaEeRD1tWmhW';-- v2
+//const CONTRACT_ADDRESS = 'KT1DwGe6b1x3iqCGMgrFykasxEErt986Rgox'; --v1
+//const TOKEN_ADDRESS = 'KT1BvYkPGPau7xDSRxLVz7DQpuWZfrXBLL27';--v1
 
 //const Tezos = new TezosToolkit('https://hangzhounet.smartpy.io');
 const Tezos = new TezosToolkit('https://ithacanet.smartpy.io');
