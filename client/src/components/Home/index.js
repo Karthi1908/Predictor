a1f3e9c319f702935ad70983f9d9ce565a0d045d
Karthik <33004854+Karthi1908@users.noreply.github.com>
2022-05-01 15:27:39 +0800

    Add files via upload

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Box, Container, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Container, Text, useColorModeValue, Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,  } from '@chakra-ui/react';
 import PredictionContext from '../../helper/PredictionContext';
 import { useNavigate } from 'react-router-dom';
 
@@ -20,6 +23,22 @@ export default function Home() {
       maxHeight="1000vh"
       padding="5vh"
     >
+	<Accordion allowToggle margin="6">
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1">Read this to get familiar with Predictor Platform</Box>
+            </AccordionButton>
+          </h2>
+          <AccordionPanel p="6"> "Predictor is for informational and educational purposes only. We take no custody of anyone's money or cryptocurrency. <br/> 
+								Read this <a href = 'https://medium.com/coinmonks/predictor-da7efb0163ca/' target="_blank"> <b>article</b></a> to know more about Predictor.<br/> 
+								Read this <a href = 'https://medium.com/@karthi1908/predictor-c90398056061' target= "_blank"><b>article</b></a> to get started on Predictor <br/>
+								Read this <a href = 'https://medium.com/@karthi1908/predictor-d01e6c67de0e/' target="_blank"> <b>article</b></a> to understand how to use predictor to predict events.<br/> 
+								Predictor displays existing markets live on the Tezos blockchain and is a graphical user interface for both visualizing data and market trends from on-chain activity, and interacting with said smart contracts directly via your Web 3 enabled wallet."</AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+	  
+	
       <Box display="flex" flexDirection="row" flexWrap="wrap">
         {predictionsArray.map((pred, i) => {
           return (
